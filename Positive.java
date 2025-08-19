728900db47414909063c204da523ba58f4d1335c
manishbeth18 <manish.beth@gmail.com>
2025-08-19 18:56:19 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Postive{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int a = sc.nextInt();
+        if(a>0){
+            System.out.println("The Entered number is positive.");
+                }
+                else{
+                    System.out.println("The Entered number is negative.");
+                }
+                
+    }
+}
